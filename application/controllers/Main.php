9f9f07ad28b6681d5969b7694f285187cf0bd814
Deona <deona.secreve@outlook.com>
2017-09-11 14:03:57 +0200

    password resetting

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -142,4 +142,87 @@ class Main extends CI_Controller {
                     redirect(site_url().'/main/');
                 }        
         }
+
+        public function forgot()
+        {
+            
+            $this->form_validation->set_rules('email', 'Email', 'required|valid_email'); 
+            
+            if($this->form_validation->run() == FALSE) {
+                $this->load->view('templates/header');
+                $this->load->view('pages/forgot');
+                $this->load->view('templates/footer');
+            }else{
+                $email = $this->input->post('email');  
+                $clean = $this->security->xss_clean($email);
+                $userInfo = $this->user_model->getUserInfoByEmail($clean);
+                
+                if(!$userInfo){
+                    $this->session->set_flashdata('flash_message', 'We cant find your email address');
+                    redirect(site_url().'/main/login');
+                }   
+                
+                if($userInfo->status != $this->status[1]){ //if status is not approved
+                    $this->session->set_flashdata('flash_message', 'Your account is not in approved status');
+                    redirect(site_url().'/main/login');
+                }
+                
+                //build token 
+                
+                $token = $this->user_model->insertToken($userInfo->id);                    
+                $qstring = $this->base64url_encode($token);                      
+                $url = site_url() . '/main/reset_password/token/' . $qstring;
+                $link = '<a href="' . $url . '">' . $url . '</a>'; 
+                
+                $message = '';                     
+                $message .= '<strong>A password reset has been requested for this email account</strong><br>';
+                $message .= '<strong>Please click:</strong> ' . $link;             
+                echo $message; //send this through mail
+                exit;
+                
+            }
+            
+        }
+
+        public function reset_password()
+        {
+            $token = $this->base64url_decode($this->uri->segment(4));         
+            $cleanToken = $this->security->xss_clean($token);
+            
+            $user_info = $this->user_model->isTokenValid($cleanToken); //either false or array();               
+            
+            if(!$user_info){
+                $this->session->set_flashdata('flash_message', 'Token is invalid or expired');
+                redirect(site_url().'/main/login');
+            }            
+            $data = array(
+                'firstName'=> $user_info->first_name, 
+                'email'=>$user_info->email,                
+                'token'=>base64_encode($token)
+            );
+           
+            $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+            $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');              
+            
+            if ($this->form_validation->run() == FALSE) {   
+                $this->load->view('templates/header');
+                $this->load->view('pages/reset_password', $data);
+                $this->load->view('templates/footer');
+            }else{
+                                
+                $this->load->library('password');                 
+                $post = $this->input->post(NULL, TRUE);                
+                $cleanPost = $this->security->xss_clean($post);                
+                $hashed = $this->password->create_hash($cleanPost['password']);                
+                $cleanPost['password'] = $hashed;
+                $cleanPost['user_id'] = $user_info->id;
+                unset($cleanPost['passconf']);                
+                if(!$this->user_model->updatePassword($cleanPost)){
+                    $this->session->set_flashdata('flash_message', 'There was a problem updating your password');
+                }else{
+                    $this->session->set_flashdata('flash_message', 'Your password has been updated. You may now login');
+                }
+                redirect(site_url().'/main/login');                
+            }
+        }
 } 
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -132,5 +132,17 @@ class User_model extends CI_Model {
         $this->db->where('id', $id);
         $this->db->update('users', array('last_login' => date('Y-m-d h:i:s A')));
         return;
-    } 
+    }
+
+    public function getUserInfoByEmail($email)
+    {
+        $q = $this->db->get_where('users', array('email' => $email), 1);  
+        if($this->db->affected_rows() > 0){
+            $row = $q->row();
+            return $row;
+        }else{
+            error_log('no user found getUserInfo('.$email.')');
+            return false;
+        }
+    }
 }
