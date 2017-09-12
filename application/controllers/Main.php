40aeea8a32afd5cfa8a44dc6a0f59052696e46c2
Jurre Kon <99034766@mydavinci.nl>
2017-09-12 09:33:57 +0200

    created session

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -7,7 +7,8 @@ class Main extends CI_Controller {
         function __construct(){
             parent::__construct();
             $this->load->model('User_model', 'user_model', TRUE);
-            $this->load->library('form_validation');    
+            $this->load->library('form_validation');
+            $this->load->library('session');
             $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
             $this->status = $this->config->item('status'); 
             $this->roles = $this->config->item('roles');
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,7 @@
  			</div><!--row-->   
     </div><!-- /container -->  
-    
+    <?php
+    	var_dump($_SESSION);
+    ?>
     </body>
 </html>
