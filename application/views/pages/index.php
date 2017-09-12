5ba88e5ff0215eae639449c8ab353d3226f8d659
oguzhanarslan <oguzhan28@hotmail.nl>
2017-09-12 11:01:47 +0200

    logout/register

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,4 +1,8 @@
 <div class="col-lg-2 col-lg-offset-2">
 <h1>Welcome</h1>
+<?php if($this->session->userdata('id')==''){ ?>
 <p>Click to <a href="<?php echo site_url();?>/main/register">Register</a></p>
+<?php }else{ ?>
+<p>Click to <a href="<?php echo site_url();?>/main/logout">Logout</a></p>
+<?php } ?>
 </div>
