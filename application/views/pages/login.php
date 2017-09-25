263ffae7bb76b27b6d1997738ce1a148e5501b6a
oguzhanarslan <oguzhan28@hotmail.nl>
2017-09-25 09:54:54 +0200

    Revert "flash message"
    This reverts commit 08b8707e8a57237e5a9277aba707ce7445996345.

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,4 +1,4 @@
-   <div class="col-lg-4 col-lg-offset-4">
+<div class="col-lg-4 col-lg-offset-4">
     <h2>Please login</h2>
     <?php $fattr = array('class' => 'form-signin');
          echo form_open(site_url().'/main/login/', $fattr); ?>
