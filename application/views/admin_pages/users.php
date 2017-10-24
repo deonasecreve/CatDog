3fdff43b3ef0f064cd349cb0d74c3571419fc8a8
Jurre Kon <99034766@mydavinci.nl>
2017-10-24 11:46:44 +0200

    bugfix

--- application/views/admin_pages/users.php
+++ application/views/admin_pages/users.php
@@ -26,8 +26,8 @@
 									<td><?=$row['first_name']?></td>
 									<td><?=$row['last_name']?></td>
 									<td><?=$row['role']?></td>
-									<td><a href="<?php echo site_url();?>/admin/edit/<?php echo $row['id']; ?>"><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></a></td>
-						    		<td><a href="<?php echo site_url();?>/admin/delete/<?php echo $row['id']; ?>"><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><span class="glyphicon glyphicon-trash"></span></button></p></a></td>
+									<td><a href="<?php echo site_url();?>/admin/edit/<?php echo $row['id']; ?>"><p data-placement="top" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></a></td>
+						    		<td><a href="<?php echo site_url();?>/admin/delete/<?php echo $row['id']; ?>"><p data-placement="top" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete"><span class="glyphicon glyphicon-trash"></span></button></p></a></td>
 								</tr>
 
 	<?php
